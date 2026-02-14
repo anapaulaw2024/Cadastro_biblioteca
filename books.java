a8179b1c780ca8044d9ae2891c9c7fc3181cda9e
AnaWulCard <156938112+anapaulaw2024@users.noreply.github.com>
2026-02-14 19:36:24 -0300

    Início
    Código básico

---
+++ books.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.util.Scanner;
+
+public class books {
+    public static void main(String[]args){
+        Scanner livro = new;
+        Scanner dataLancamento = new;
+        String status = ["em atraso de devolução","disponível"];
+        livro = System.out.println("Digite aqui o livro que deseja cadastrar");
+        livro = dataLancamento = System.out.println("Digite a data de publicação do livro(dd/mm/aa)");
+        if(status[0]){
+                String taxa = "R$2,00"
+
+    }else{
+            System.out.print("Livro disponível para retirada");
+        }
+        String cadastrado = (livro+dataLancamento+status++1);
+}
